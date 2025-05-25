41bddee4f72c24efefcd0842e8d98f100a2f3c92
Tyler <tylermwight@gmail.com>
2025-05-25 07:10:39 +0000

    clean up

--- main.py
+++ main.py
@@ -12,11 +12,8 @@ load_dotenv(dotenv_path)
 DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
 
 
-url = 'https://www.youtube.com/watch?v=xcEvNwQ7v-U'
-
-
 intents = discord.Intents.default()
-intents.message_content = True  # Required to read text commands
+intents.message_content = True
 
 bot = commands.Bot(command_prefix='!', intents=intents)
 
@@ -47,18 +44,15 @@ async def play(ctx, url: str):
     dl_notif_source = discord.FFmpegPCMAudio('dlsong.mp3')
     voice_client.play(dl_notif_source)
     
-    #success, message = downloader.get_song(url)
+    #download the song, return the file name or error message
     success, message = await asyncio.to_thread(downloader.get_song, url)
     
     if not success:
         await ctx.send(message)
 
 
-    # Play audio file
     audio_source = discord.FFmpegPCMAudio(message)
     voice_client.play(audio_source)
-
-    # Wait for the audio to finish
     while voice_client.is_playing():
         await asyncio.sleep(1)
 
@@ -66,16 +60,13 @@ async def play(ctx, url: str):
 
 @bot.command()
 async def stop(ctx):
-    # Check if bot is connected to a voice channel
     if ctx.voice_client is None:
         await ctx.send("I'm not connected to a voice channel.")
         return
 
-    # Stop audio if playing
     if ctx.voice_client.is_playing():
         ctx.voice_client.stop()
 
-    # Disconnect
     await ctx.voice_client.disconnect()
     await ctx.send("Stopped playing and disconnected.")
 
